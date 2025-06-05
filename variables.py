e71c61fd0a660bbcec8d1988cc55c7614cfa232b
meltjacTechs <ebwire02@gmail.com>
2025-06-06 00:48:19 +0300

    basic python variables

--- variables.py
+++ variables.py
@@ -1,2 +1,7 @@
+# Basics of Python variables
+
 students_count = 1000
-print (students_count)
+rating = 4.99
+is_published = True
+course_name = "Python Programming"
+print ( rating, course_name, students_count )
